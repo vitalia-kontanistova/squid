f7af3fc1585ce22e7653ea7a5cad640e666641a3
vitalia-kontanistova <v.konti@outlook.com>
2020-11-14 21:09:57 +0300

    Initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+	return (
+		<footer className="footer">
+		© SquidWords 2020 - See u in sea!
+	</footer>
+	);
+}
+export default Footer;
+ 
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+	return (
+		<header className="header">
+			<div className="header__logo">
+				<img src="https://i.pinimg.com/originals/86/b2/48/86b2489d04f827da9e36f05a237292dd.jpg" alt="logo"></img>
+			</div>
+			<div className="header__title">SquidWords</div>
+		</header>
+	);
+}
+export default Header;
+ 
